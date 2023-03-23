20e4e267c281862d08f893b6765eb2b92a1c324d
dyslexicsteak <49301588+dyslexicsteak@users.noreply.github.com>
2023-03-23 12:17:33 +0300

    Add Options

--- src/cli.rs
+++ src/cli.rs
@@ -1,10 +1,20 @@
 use std::path::PathBuf;
 
-use clap::Parser;
+use clap::{Parser, ValueEnum};
 
 #[derive(Debug, Parser)]
-#[command(version)]
+#[command(about, version)]
 pub(crate) struct Cli {
     pub(crate) directory: PathBuf,
     pub(crate) duration: Option<String>,
+    #[arg(long)]
+    pub(crate) output: Option<Output>,
+    #[arg(long, default_value_t = false)]
+    pub(crate) dry_run: bool,
+}
+
+#[derive(Clone, Debug, ValueEnum)]
+pub(crate) enum Output {
+    Quiet,
+    Verbose,
 }
